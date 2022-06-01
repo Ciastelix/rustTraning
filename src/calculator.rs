bd5fc29c7a7037fa93414beaa4430d2cca6cd44a
Ciastelix <tenlegitmati@gmail.com>
2022-06-01 15:58:03 +0200

    calculator created

--- src/calculator.rs
+++ src/calculator.rs
@@ -1,5 +1,3 @@
-use std::arch::x86_64::_MM_FROUND_TO_NEG_INF;
-
 fn dodaj(a: f32, b: f32) -> f32 {
     return a + b;
 }
@@ -12,16 +10,30 @@ fn pomoz(a: f32, b: f32) -> f32 {
 fn podziel(a: f32, b: f32) -> f32 {
     return a / b;
 }
-fn main() {
-    let mut znak=String::from("*");
-    let mut wynik: f32;
-    let mut num1: f32 = 1.1;
-    let mut num2: f32 = 2.3;
-    match znak.as_str() {
-        "+" => wynik = dodaj(num1, num2),
-        "-" => wynik = odejmij(num1, num2),
-        "*" => wynik = pomoz(num1, num2),
-        "/" => wynik = podziel(num1, num2),
+fn wybierz(a: f32, b: f32, znak: char) -> f32 {
+    let wynik: f32;
+    match znak {
+        '+' => wynik = dodaj(a, b),
+        '-' => wynik = odejmij(a, b),
+        '*' => wynik = pomoz(a, b),
+        '/' => {
+            if b == 0.0 {
+                println!("Nie można dzielić przez 0");
+                wynik = 0.0;
+            } else {
+                wynik = podziel(a, b);
+            }
+        }
+        _ => {
+            println!("Niepoprawny znak!");
+            wynik = 0.0;
+        }
     }
-    println!("{} {} {} = {}", num1, znak, num2, wynik);
+    return wynik;
+}
+fn main() {
+    let znak: char = '*';
+    let num1: f32 = 1.1;
+    let num2: f32 = 2.3;
+    println!("{} {} {} = {}", num1, znak, num2, wybierz(num1, num2, znak));
 }
